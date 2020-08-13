3704f204c0ea4789386656aa39de11bf568e5e53
mario-otz19 <mario_otz@hotmail.com>
2020-08-12 23:37:36 -0500

    docs agregado

---
+++ docs/precache-manifest.3e1af43a8b6923ac7df42d7c681ebb37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a1bc38ebdd3c916f1c03b1f2476b77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564a4a30bf1af8e9c349",
+    "url": "/static/css/main.db25ecf9.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564a4a30bf1af8e9c349",
+    "url": "/static/js/main.2b4e6b55.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
